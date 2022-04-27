94dc49747b95522251be3d3c4d3c528f8514b8e0
aca-polytech <arnaud.castelltort@umontpellier.fr>
2022-04-27 14:45:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@
 // Fichier généré automatiquement par polyconquest :
 // Veuillez utiliser ce fichier pour envoyer votre code
 // -------------------------------
-console.log("move 1")
+console.log("move 0 1")
